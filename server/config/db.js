2c711a9d0107d1d99eddc484bc3cb51a98d1d426
rickynguyen1707 <55204776+rickynguyen1707@users.noreply.github.com>
2020-10-25 14:28:29 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI" : "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://ricky:haKlKizmPUWmAh3f@mongodbserver.27gwz.mongodb.net/book_store?retryWrites=true&w=majority"
 }
